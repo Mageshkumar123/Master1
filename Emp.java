02d02868e10ad989174d3e15bf1c584ba3a6951e
Magesh <mahesh00666@gmail.com>
2022-01-11 18:32:22 +0530

    python

--- Emp.java
+++ Emp.java
@@ -1,12 +1,20 @@
-package org.test;
+package org.test.Master1;
 
 public class Emp {
 	private void m1() {
 System.out.println("java");
+
+
+
+
+	}
+	private void m3() {
+System.out.println("python");
 	}
 	public static void main(String[] args) {
 		Emp e=new Emp();
 		e.m1();
+		e.m3();
 		
 	}
 
