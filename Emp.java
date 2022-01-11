ca11fd4247c52cdfd035b9cd85a1b0374f52df21
Magesh <mahesh00666@gmail.com>
2022-01-11 18:02:55 +0530

    git done

---
+++ Emp.java
@@ -0,0 +1,13 @@
+package org.test;
+
+public class Emp {
+	private void m1() {
+System.out.println("java");
+	}
+	public static void main(String[] args) {
+		Emp e=new Emp();
+		e.m1();
+		
+	}
+
+}
