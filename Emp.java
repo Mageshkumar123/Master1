3a80d462d8d36162b0a57c33706c4b93df190ea4
Magesh <mahesh00666@gmail.com>
2022-01-11 18:47:32 +0530

    suceess

--- Emp.java
+++ Emp.java
@@ -1,13 +1,16 @@
-package org.test;
+package org.test.Master1;
 
 public class Emp {
 	private void m1() {
 System.out.println("java");
 	}
+	private void m4() {
+System.out.println("123");
+	}
 	public static void main(String[] args) {
 		Emp e=new Emp();
 		e.m1();
-		
+		e.m4();
 	}
 
 }
