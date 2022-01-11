d85a09d95b905c31da09a019c760693fd2b51b31
Magesh <mahesh00666@gmail.com>
2022-01-11 18:20:13 +0530

    git started

--- Emp.java
+++ Emp.java
@@ -1,12 +1,18 @@
-package org.test;
+package org.test.Master1;
 
 public class Emp {
 	private void m1() {
 System.out.println("java");
 	}
+	private void m2() {
+		System.out.println("selenium");
+
+	}
 	public static void main(String[] args) {
 		Emp e=new Emp();
 		e.m1();
+		e.m2();
+		
 		
 	}
 
